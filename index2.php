173daaf73f1e299300ba7fafefb55d7c4016ccb5
Lyah Motta <lyah.smo@gmail.com>
2024-07-03 12:10:45 -0500

    index2 in process to be well implemented

--- index2.php
+++ index2.php
@@ -2,6 +2,10 @@
 <?php
 echo "entra";
 
+require('PHPMailer.php');
+require('SMTP.php');
+require('exception.php');
+
 class ICS {
 	public $data;
 	public $name;
@@ -10,9 +14,11 @@ class ICS {
 		$this->name = $name;
 		$this->data = "BEGIN:VCALENDAR\nVERSION:2.0\nMETHOD:PUBLISH\nBEGIN:VEVENT\nDTSTART:".date("Ymd\THis\Z",strtotime($start))."\nDTEND:".date("Ymd\THis\Z",strtotime($end))."\nLOCATION:".$location."\nTRANSP: OPAQUE\nSEQUENCE:0\nUID:\nDTSTAMP:".date("Ymd\THis\Z")."\nSUMMARY:".$name."\nDESCRIPTION:".$description."\nPRIORITY:1\nCLASS:PUBLIC\nBEGIN:VALARM\nTRIGGER:-PT10080M\nACTION:DISPLAY\nDESCRIPTION:Reminder\nEND:VALARM\nEND:VEVENT\nEND:VCALENDAR\n";
 	}
+
 	function save() {
 		file_put_contents($this->name.".ics",$this->data);
 	}
+
 	function show() {
 		header("Content-type:text/calendar");
 		header('Content-Disposition: attachment; filename="'.$this->name.'.ics"');
